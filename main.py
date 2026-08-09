4dff5dded194eda1ac530d4845d9f3b1bc77d7ea
Arif Molla <xmarthacker2@gmail.com>
2026-08-09 14:04:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def ping():
 # ========== Render-এর জন্য পোর্ট ==========
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port)
