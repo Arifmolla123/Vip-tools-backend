68b397088c324232542385c23afd374a865ba1f3
Arif Molla <xmarthacker2@gmail.com>
2026-09-26 21:22:34 +0530

    Update formatter.py

--- md_tools/formatter.py
+++ md_tools/formatter.py
@@ -2,7 +2,7 @@ import requests
 import logging
 
 logger = logging.getLogger(__name__)
-BOT_TOKEN = "8193376363:AAHTTtXNtQqCZ2a_Hd1cpus1Z2iz6kOORo"
+BOT_TOKEN = "8193376363:AAFyMyVmK7gryI4H1ZxZOobwFt_wzeFwJrM"
 
 def handle_commands(msg):
     text = msg.get('text', '')
